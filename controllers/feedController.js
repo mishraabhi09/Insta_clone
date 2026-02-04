61d5bc1f5bfa32bd2986b1b473433fe584127a0e
mishraabhi09 <abhiimishra1502@gmail.com>
2026-02-04 22:28:16 +0530

    Feed controller Finished

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -93,17 +93,133 @@ const deleteFeed = async (req, res) => {
     )
 };
 
-const likeFeed = async () => {
-     
-    const {id : feedId} = req.params;
+// controller function to like the feed-->>
+const likeFeed = async (req, res) => {
 
-    let feed = await Feed.findById({_id : feedId});
+    const { id: feedId } = req.params;
+
+    let feed = await Feed.findById({ _id: feedId });
+
+    if (feed.likes.includes(req.user.userId)) {
+        feed = await Feed.findByIdAndUpdate(
+            { _id: feedId },
+            {
+                $pull: { likes: req.user.userId },
+            },
+            {
+                new: true,
+            }
+        );
+    }
+    else {
+        feed = await Feed.findByIdAndUpdate(
+            { _id: feedId },
+            {
+                $push: { likes: req.user.userId },
+            },
+            {
+                new: true,
+            }
+        );
+    }
+
+    res.status(StatusCodes.OK).json(
+        {
+            feed
+        }
+    )
+};
+
+// controller function to getAllFollowingFeed-->>
+const getAllFollowingFeed = async (req, res) => {
+
+    let followingFeeds = await Feed.find(
+        {
+            postedBy: { $in: req.user.userFollowing },
+        }).sort({ created: -1 })
+        .populate("postedBy", "_id username fullName email avatar bio")
+        .populate("comments.commentedBy", "_id username fullName email avatar bio");
+
+    followingFeeds = followingFeeds.map((data, index) => {
+        return { ...data.doc, liked: data.likes.includes(req.user.userId) };
+    });
+
+    res.status(StatusCodes.OK).json(
+        {
+            followingFeeds
+        }
+    )
+}
+
+// controller function to find the currentuserFeed-->>
+const currentUserFeed = async (req, res) => {
+
+    const feed = await Feed.find({ postedBy: req.user.userId })
+        .sort({ created: -1 })
+        .populate("postedBy", "_id username fullName email avatar bio")
+        .populate("commnets.commentedBy", "_id username fullName email avatar bio");
+
+    res.status(StatusCodes.OK).json(
+        {
+            feed
+        }
+    )
+}
+
+// controller function to commentonFeed-->>
+const commentOnFeed = async (req, res) => {
+
+    const { id: postId } = req.params;
+    const { comment } = req.body;
+
+    const commentObj =
+    {
+        comment,
+        commentedBy: req.user.userId,
+    };
+
+    const commentFeed = await Feed.findByIdAndUpdate(
+        {
+            _id: postId,
+        },
+        {
+            $push: { comment: commentObj },
+        },
+        {
+            new: true,
+        }
+    );
+
+    res.status(StatusCodes.OK).json(
+        {
+            commentFeed
+        }
+    );
+};
+
+// controller function to getAllComments-->>
+const getAllComments = async (req, res) => {
+
+    const { id: postId } = req.params;
+
+    const feedComment = await Feed.findOne({ _id: postId })
+        .sort({ created: -1 })
+        .populate("comment.commentedBy", "_id username fullName email avatar bio");
+
+    res.status(StatusCodes.OK).json({
+        feedComment,
+    })
 }
 
 module.exports = {
     getAllFeed,
     createFeed,
     getFeed,
-    deleteFeed
+    deleteFeed,
+    likeFeed,
+    getAllFollowingFeed,
+    currentUserFeed,
+    commentOnFeed,
+    getAllComments
 }
 
